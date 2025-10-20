bf3fa43d03578e7f4fcb6dbe7331b5965f6c8d47
Professor Akram <professorakramburris@gmail.com>
2025-10-20 13:51:58 -0500

    feat: mushroom

---
+++ transform-unity/Assets/Scripts/ShrinkGrow.cs
@@ -0,0 +1,60 @@
+/************************************************************
+* COPYRIGHT:  Year
+* PROJECT: Name of Project or Assignment
+* FILE NAME: ShrinkGrow.cs
+* DESCRIPTION: Short Description of script.
+*                   
+* REVISION HISTORY:
+* Date [YYYY/MM/DD] | Author | Comments
+* ------------------------------------------------------------
+* 2000/01/01 | Your Name | Created class
+*
+*
+************************************************************/
+ 
+using UnityEngine;
+ 
+
+public class ShrinkGrow : MonoBehaviour
+{
+    
+    // ===== Hidden Fields =====
+    private Vector3 _startingScale; 
+
+    // Awake is called once on initialization (before Start)
+    private void Awake()
+    {
+        
+        
+    } //end Awake()
+ 
+ 
+    // Start is called once before the first Update
+    private void Start()
+    {
+        
+        
+    } //end Start()
+ 
+ 
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        
+    } //end Update()
+ 
+ 
+    /// <summary>
+    /// A custom method example.
+    /// </summary>
+    /// <param name="exampleParameter"> A parameter that demonstrates passing data to the method.
+    ///</param>
+    private void CustomMethod(int exampleParameter)
+    {
+        
+        
+    }//end CustomMethod(int)
+ 
+ 
+}//end ShrinkGrow
